6f8b83076782180e74b02f9edfa1084bba19b3dc
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-07-26 00:11:33 -0300

    creating an environment variable in the project

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 const app = express(); //In this app variable we are going to have all the functionality required by the server
+
+dotenv.config(); // setting the environment variables
+
+connectDB();
 
 app.use('/', (req, res) => {
   res.send('Hello Word');
 });
 
-app.listen(4000, () => {
-  console.log('Server working correctly on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server working correctly on port ${PORT} :)`);
 });
