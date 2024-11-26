1bbf0e84d60d9de06513d903d4255e559f328045
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-11-26 20:55:39 -0300

    Adding CORS options for domains

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import veterinaryRoute from './routes/veterinaryRoute.js';
@@ -10,6 +11,19 @@ dotenv.config(); // setting the environment variables
 
 connectDB();
 
+const allowedDomains = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedDomains.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use('/api/veterinary', veterinaryRoute);
 app.use('/api/patient', patientRoute);
 
