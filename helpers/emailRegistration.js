180d60c7b3a1a3b749a09224ec4148bfb46a4d31
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-11-28 21:49:59 -0300

    Instaling nodemailer + mailtrap

---
+++ helpers/emailRegistration.js
@@ -0,0 +1,13 @@
+import nodemailer from 'nodemailer';
+
+const emailRegistration = () => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+};
+export default emailRegistration;
